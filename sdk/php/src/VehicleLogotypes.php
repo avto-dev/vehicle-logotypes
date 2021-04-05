bc9de923d57ce0a0ada23d40fa9379eb1e36d6aa
IgorKorytin <korytin.i@avtocod.ru>
2021-04-05 13:43:45 +0500

    Add exception

--- sdk/php/src/VehicleLogotypes.php
+++ sdk/php/src/VehicleLogotypes.php
@@ -9,12 +9,14 @@ final class VehicleLogotypes extends \Illuminate\Support\Collection
     /**
      * Create new instance with data.
      *
+     * @throws \JsonException
+     *
      * @return VehicleLogotypes<string, array<string, mixed>>
      */
     public static function create(): self
     {
         return new self(
-            \json_decode((string) \file_get_contents(__DIR__ . '/../../../src/vehicle-logotypes.json'), true)
+            \json_decode((string) \file_get_contents(__DIR__ . '/../../../src/vehicle-logotypes.json'), true, 512, JSON_THROW_ON_ERROR)
         );
     }
 }
